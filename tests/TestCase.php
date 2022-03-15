c6416d7773cf7a808a46206224b3a59a58d48b92
xiaoxuan6 <1527736751@qq.com>
2022-03-15 13:13:34 +0800

    add tests

---
+++ tests/TestCase.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) vinhson <15227736751@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vinhson\LaravelPackageSkeleton\Tests;
+
+use Illuminate\Foundation\Application;
+use Vinhson\Elasticsearch\ElasticsearchServiceProvider;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ElasticsearchServiceProvider::class
+        ];
+    }
+}
